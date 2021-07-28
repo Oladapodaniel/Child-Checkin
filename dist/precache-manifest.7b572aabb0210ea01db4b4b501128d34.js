41237c142370162e43210edf4a226e2fbe3968ec
Oladapo Daniel <oladapodaniel10@gmail.com>
2021-07-28 22:48:35 +0100

    added dist

---
+++ dist/precache-manifest.7b572aabb0210ea01db4b4b501128d34.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e042220cf3c0b85be77b",
+    "url": "/css/app.cd514e2b.css"
+  },
+  {
+    "revision": "8444ecf99968a76a0ecf",
+    "url": "/css/chunk-vendors.e1b2a8c0.css"
+  },
+  {
+    "revision": "66f475a47ec8aa1fc95f",
+    "url": "/css/defaultmessage.3ef3143d.css"
+  },
+  {
+    "revision": "5bf2b16b3320c0b523ad",
+    "url": "/css/sentemails.034555c0.css"
+  },
+  {
+    "revision": "95360015d06d6a013cd2",
+    "url": "/css/workflow.4322f686.css"
+  },
+  {
+    "revision": "121254f73060bcbb53ca13258dbd134f",
+    "url": "/fonts/primeicons.121254f7.ttf"
+  },
+  {
+    "revision": "3b6e3706f42d8876fe364ab4d75683fd",
+    "url": "/fonts/primeicons.3b6e3706.woff"
+  },
+  {
+    "revision": "b0f5d02f4e70967dcc8162e938484366",
+    "url": "/fonts/primeicons.b0f5d02f.eot"
+  },
+  {
+    "revision": "2b29fa19403d5df6764636d772fb7320",
+    "url": "/img/Group-157.2b29fa19.svg"
+  },
+  {
+    "revision": "55df5dc1c5d5ab3ed7d8c2617ad5e399",
+    "url": "/img/Group-16991.55df5dc1.svg"
+  },
+  {
+    "revision": "e814908491da450ed3c119b187c017d3",
+    "url": "/img/Icon-ionic-ios-person.e8149084.svg"
+  },
+  {
+    "revision": "62354df5c4b02e3c3f52eacf33111a81",
+    "url": "/img/No-Upcoming-Events.62354df5.png"
+  },
+  {
+    "revision": "41eb7ee3c44bef16ac43ac14538e8504",
+    "url": "/img/child1.41eb7ee3.png"
+  },
+  {
+    "revision": "e12841dc0a5386ec699b207a7aadb972",
+    "url": "/img/clapping_hands.e12841dc.gif"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/img/color.c7a33805.png"
+  },
+  {
+    "revision": "0aec3adea4a4a2a47e421adce29ebdc1",
+    "url": "/img/congrats.0aec3ade.jpeg"
+  },
+  {
+    "revision": "6f1cc1748cf38134306a7be8458e55ab",
+    "url": "/img/dashboard.6f1cc174.svg"
+  },
+  {
+    "revision": "8e6676ed5193b46ad414de0c01273534",
+    "url": "/img/down-emoji.8e6676ed.png"
+  },
+  {
+    "revision": "c9fdb2c46076f7d0c34d80a80367d4ab",
+    "url": "/img/family-silhouette.c9fdb2c4.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4f2d1389a8d258af1ae31ffbe3953ede",
+    "url": "/img/people-empty.4f2d1389.svg"
+  },
+  {
+    "revision": "1f66ff23bc9a341da44980deac73a3a6",
+    "url": "/img/phone-import.1f66ff23.svg"
+  },
+  {
+    "revision": "25954ab5e32c01d577dcafd035597f5d",
+    "url": "/img/primeicons.25954ab5.svg"
+  },
+  {
+    "revision": "c10d1dfc79e96a664ef922c1579f9037",
+    "url": "/img/smile.c10d1dfc.jpg"
+  },
+  {
+    "revision": "d0969a420297e5f12ca3e08c655699cc",
+    "url": "/img/worship-service.d0969a42.jpeg"
+  },
+  {
+    "revision": "e256b812b1d5bea5af94b18c6d510ad5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e042220cf3c0b85be77b",
+    "url": "/js/app.42cbf1c8.js"
+  },
+  {
+    "revision": "8444ecf99968a76a0ecf",
+    "url": "/js/chunk-vendors.3cac0aeb.js"
+  },
+  {
+    "revision": "66f475a47ec8aa1fc95f",
+    "url": "/js/defaultmessage.c0f2c737.js"
+  },
+  {
+    "revision": "5bf2b16b3320c0b523ad",
+    "url": "/js/sentemails.97fdf102.js"
+  },
+  {
+    "revision": "11c9b2e4cdccc6d7b49e",
+    "url": "/js/sentemails~workflow.f0a2ebc7.js"
+  },
+  {
+    "revision": "95360015d06d6a013cd2",
+    "url": "/js/workflow.46d418f2.js"
+  },
+  {
+    "revision": "91a0cb7958889a1c5b3751f002a45278",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
