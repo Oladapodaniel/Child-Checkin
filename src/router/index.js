02722608b57e6e3a69d4abfbe851628e7fbc1e8c
Oladapo Daniel <oladapodaniel10@gmail.com>
2021-07-28 23:32:11 +0100

    modified router

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Home from '../views/Home.vue'
 
 const routes = [
       {
-        path: '/child-checkin',
+        path: '/',
         name: 'CheckinSignin',
         component: () =>
         import( /* webpackChunkName: "workflow" */ '../views/ChildCheckinPortal/CheckinSignin.vue'),
