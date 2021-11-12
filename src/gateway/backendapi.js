dd3ad3645a89ac9c9f77a04eb9cff54d1154d42e
Oladapo Daniel <oladapodaniel10@gmail.com>
2021-11-12 18:28:47 +0100

    remove eco url

--- src/gateway/backendapi.js
+++ src/gateway/backendapi.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-    // baseURL: 'https://ecofaith.azurewebsites.net/', //local
     // baseURL: 'https://churchplusv3coreapi.azurewebsites.net/',
     // baseURL: 'https://unionfaith.azurewebsites.net/',
     baseURL: 'https://churchplusapi3v.azurewebsites.net/',
